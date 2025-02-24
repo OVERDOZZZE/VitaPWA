545a06b7e23048bf6e28f7e31a224679b562400f
OVERDOZZZE <saparbekov113@gmail.com>
2025-02-23 21:03:58 -0600

    commit

---
+++ staticfiles/profile/js/login_sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "ecommerce-cache-v1";
+const urlsToCache = [
+    "/",
+//    "/static/profile/css/base.css",
+//    "/static/shop/js/main.js",
+    "/static/images/icon_192.png",
+    "/static/images/icon_512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
